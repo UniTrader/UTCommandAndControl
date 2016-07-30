51af352d1519a5529da7123c7ddf2a137090e484
UniTrader <unitrader@gmx.de>
2016-07-30 20:09:42 +0200

    made linux symlink Script working

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # param $1: X Rebirth Extensions Directory
-if [ -d $1 ]; then
-  for d in */; do
-    ln -s $1 $d
+#if [ -d $1 ]; then
+  for d in ./*/; do
+    ln -s "$PWD/$d" "$1"
   done
-fi
+#fi
