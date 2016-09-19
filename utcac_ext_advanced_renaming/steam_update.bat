c91ba6e2d04cd7fee1322e91e884d0c4cfbf76d1
UniTrader <unitrader87@googlemail.com>
2016-09-20 00:16:25 +0200

    Stam Batch Update for Advanced Renaming added
    and some internal re-structure

---
+++ utcac_ext_advanced_renaming/steam_update.bat
@@ -0,0 +1,6 @@
+"D:\SteamLibrary\SteamApps\common\X Rebirth Tools\XRCatTool.exe" -in . -include md/ t/ -out ext_01.cat -dump
+"D:\SteamLibrary\SteamApps\common\X Rebirth Tools\XRCatTool.exe" -in subst_01 -out subst_01.cat -dump
+"D:\SteamLibrary\SteamApps\common\X Rebirth Tools\WorkshopTool.exe" update -path . -contentdef .\content_steam.xml -minor -changenote "forgot something."
+del ext_01.cat
+del ext_01.dat
+@pause
