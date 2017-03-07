52bea8f3958f17b04f2a597afdce4ce8f09514c4
UniTrader <unitrader@gmx.de>
2017-03-07 20:48:31 +0100

    ..

--- utcac_base/ui/ita_genericui_ut_mod.lua
+++ utcac_base/ui/ita_genericui_ut_mod.lua
@@ -95,7 +95,7 @@ function menu.fetch()
 	
 	-- Calculate Dynamic Row Size if we have a dynamic (-1) Entry
 	local index = nil
-	local totalwidth = -5 -- set to -5 to have 1 pixel tolerance to the 1200 pixel limit and to omit one spacer between columns with a size of 4 (added for each column in the loop following)
+	local totalwidth = -6 -- set to -6 to have 2 pixel tolerance to the 1200 pixel limit and to omit one spacer between columns with a size of 4 (added for each column in the loop following)
 	for i, v in pairs(menu.data.midtable_column_sizes) do
 		if v == -1 then
 			index = i
