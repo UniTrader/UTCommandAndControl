526f020066c5047a33f818215996e011406531fc
UniTrader <unitrader@gmx.de>
2017-03-07 21:00:19 +0100

    ..

--- utcac_base/ui/ita_genericui_ut_mod.lua
+++ utcac_base/ui/ita_genericui_ut_mod.lua
@@ -104,8 +104,8 @@ function menu.fetch()
 		end
 	end
 	if index then
-		print("Found -1 Entry in column "..index.." will set it to remaining width of "..(1190 - totalwidth).."pixels - Totalwidth="..totalwidth)
-		menu.data.midtable_column_sizes[index] = 1190 - totalwidth
+		print("Found -1 Entry in column "..index.." will set it to remaining width of "..(1196 - totalwidth).."pixels - Totalwidth="..totalwidth)
+		menu.data.midtable_column_sizes[index] = 1196 - totalwidth
 		totalwidth = totalwidth + menu.data.midtable_column_sizes[index]
 	end
 	
