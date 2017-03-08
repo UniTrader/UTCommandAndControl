6109c8c6a2d9919b8187e9b060d2d2e55153347a
UniTrader <unitrader@gmx.de>
2017-03-08 21:00:14 +0100

    ..

--- utcac_base/ui/ita_genericui_ut_mod.lua
+++ utcac_base/ui/ita_genericui_ut_mod.lua
@@ -158,8 +158,9 @@ function menu.onShowMenu()
 	
 	-- Set up CreateCell Function
 	local function CreateCell(celldefinition,row,column,height,width,buttonlist)
-		print("createCell called for a "..(celldefinition[1] or "nil").." Cell")
+		if celldefinition then print("createCell called for a "..celldefinition[1].." Cell") end
 		if celldefinition == nil then
+			print("createCell called for an empty Cell")
 			return Helper.getEmptyCellDescriptor()
 		elseif celldefinition[1] == "text" then
 	-- { 'text' , 'Text' }
