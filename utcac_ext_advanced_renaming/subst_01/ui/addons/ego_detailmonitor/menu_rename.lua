7ce778952ee9461abc76008bf54851574f0c9819
UniTrader <unitrader@gmx.de>
2017-05-30 00:15:54 +0200

    Experiment: Rename Menu Keyboard

--- utcac_ext_advanced_renaming/subst_01/ui/addons/ego_detailmonitor/menu_rename.lua
+++ utcac_ext_advanced_renaming/subst_01/ui/addons/ego_detailmonitor/menu_rename.lua
@@ -184,6 +184,57 @@ function menu.onShowMenu()
 		Helper.getEmptyCellDescriptor()
 	}, nil, {1, 1, 1, 1, 1}, false, menu.transparent)
 	
+	
+	setup:addSimpleRow({'Experimental Keyboard',Helper.getEmptyCellDescriptor()}, nil, {3, 2})
+	setup:addSimpleRow({ 
+		Helper.createButton(Helper.createButtonText("1", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("2", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("3", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("4", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("5", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("6", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("7", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("8", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("9", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("0", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil)
+	}, nil, {1,1,1,1,1,1,1,1,1,1}, false, menu.transparent)
+	setup:addSimpleRow({ 
+		Helper.createButton(Helper.createButtonText("Q", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("W", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("E", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("R", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("T", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("Z", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("U", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("I", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("O", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("P", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil)
+	}, nil, {1,1,1,1,1,1,1,1,1,1}, false, menu.transparent)
+	setup:addSimpleRow({ 
+		Helper.createButton(Helper.createButtonText("A", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("S", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("D", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("F", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("G", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("H", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("J", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("K", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("L", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("Ö", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil)
+	}, nil, {1,1,1,1,1,1,1,1,1,1}, false, menu.transparent)
+	setup:addSimpleRow({ 
+		Helper.createButton(Helper.createButtonText("Y", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("X", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("C", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("V", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("B", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("N", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("M", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText(",", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText(".", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil),
+		Helper.createButton(Helper.createButtonText("-", "center", Helper.standardFont, Helper.standardFontSize, 255, 255, 255, 100), nil, false, true, 0, 0, 160, 25, nil, nil, nil, nil)
+	}, nil, {1,1,1,1,1,1,1,1,1,1}, false, menu.transparent)
+	
 	-- Expressions Help - Static Info Text
 	if ( ReadText(5554302, 6) == "All" ) or ( ReadText(5554302, 6) == "Static" ) then
 		setup:addSimpleRow({Helper.getEmptyCellDescriptor()}, nil, {5})
@@ -260,7 +311,7 @@ function menu.onShowMenu()
 		}, nil, {1, 1, 1, 1, 1}, false, menu.transparent)
 	end
 	
-	local buttondesc = setup:createCustomWidthTable({160, 160, 160, 160, 160}, false, false, false, 2, 1, 0, 150)
+	local buttondesc = setup:createCustomWidthTable({80,80,80,80,80,80,80,80,80,80}, false, false, false, 2, 1, 0, 150)
 
 	-- create tableview
 	menu.infotable, menu.selecttable, menu.buttontable = Helper.displayThreeTableView(menu, infodesc, selectdesc, buttondesc, false)
@@ -273,6 +324,63 @@ function menu.onShowMenu()
 	Helper.setButtonScript(menu, nil, menu.buttontable, 3, 1, menu.buttonRenameSubordinates)
 	Helper.setButtonScript(menu, nil, menu.buttontable, 3, 2, menu.buttonRenameSubordinatesBigShips)
 	Helper.setButtonScript(menu, nil, menu.buttontable, 3, 3, menu.buttonRenameSubordinatesSmallShips)
+	-- Experimental Keyboard Buttons
+	-- First Row Numbers
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 1, function () return widgetSystem.onDirectTextInput(null,'1') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 2, function () return widgetSystem.onDirectTextInput(null,'2') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 3, function () return widgetSystem.onDirectTextInput(null,'3') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 4, function () return widgetSystem.onDirectTextInput(null,'4') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 5, function () return widgetSystem.onDirectTextInput(null,'5') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 6, function () return widgetSystem.onDirectTextInput(null,'6') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 7, function () return widgetSystem.onDirectTextInput(null,'7') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 8, function () return widgetSystem.onDirectTextInput(null,'8') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 9, function () return widgetSystem.onDirectTextInput(null,'9') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 5, 10, function () return widgetSystem.onDirectTextInput(null,'0') end)
+	-- First Row qwertzuiop
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 1, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'Q' else 'q') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 2, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'W' else 'w') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 3, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'E' else 'e') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 4, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'R' else 'r') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 5, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'T' else 't') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 6, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'Z' else 'z') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 7, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'U' else 'u') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 8, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'I' else 'i') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 9, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'O' else 'i') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 6, 10, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'P' else 'p') end)
+	-- Second Row asdfghjklöä
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 1, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'A' else 'a') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 2, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'S' else 's') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 3, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'D' else 'd') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 4, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'F' else 'f') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 5, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'G' else 'g') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 6, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'H' else 'h') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 7, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'J' else 'j') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 8, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'K' else 'k') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 9, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'L' else 'l') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 7, 10, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'Ö' else 'ö') end)
+	-- Third Row yxcvbnm,.
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 1, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'Y' else 'y') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 2, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'X' else 'x') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 3, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'C' else 'c') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 4, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'V' else 'v') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 5, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'B' else 'b') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 6, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'N' else 'n') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 7, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'M' else 'm') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 8, function () return widgetSystem.onDirectTextInput(null,if menu.caps then ';' else ',') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 9, function () return widgetSystem.onDirectTextInput(null,if menu.caps then ':' else '.') end)
+	Helper.setButtonScript(menu, nil, menu.buttontable, 8, 10, function () return widgetSystem.onDirectTextInput(null,if menu.caps then '_' else '-') end)
+	-- Bottom Row: Special Functions
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 1, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'C' else 'y') end)
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 2, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'X' else 'x') end)
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 3, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'C' else 'c') end)
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 4, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'V' else 'v') end)
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 5, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'B' else 'b') end)
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 6, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'N' else 'n') end)
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 7, function () return widgetSystem.onDirectTextInput(null,if menu.caps then 'M' else 'm') end)
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 8, function () return widgetSystem.onDirectTextInput(null,if menu.caps then ';' else ',') end)
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 9, function () return widgetSystem.onDirectTextInput(null,if menu.caps then ':' else '.') end)
+	--Helper.setButtonScript(menu, nil, menu.buttontable, 8, 10, function () return widgetSystem.onDirectTextInput(null,if menu.caps then '_' else '-') end)
+	-- End Keyboard Keys
 	if false and ( extensionSettings["utfactionlogos"].enabled or extensionSettings["ws_329415910"].enabled ) and extensionSettings["utcac_ext_advanced_renaming_user"].enabled then
 		Helper.setButtonScript(menu, nil, menu.buttontable, 4, 1, menu.buttonSetLogoFromSuperior)
 		Helper.setButtonScript(menu, nil, menu.buttontable, 5, 1, menu.buttonSetLogoCurrent)
